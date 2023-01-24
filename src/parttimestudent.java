99ce4efbe7bd39fade6a6d1aac56b0efca6071e0
Ali Ibrahim Maznani <maznani@sheridancollege.ca>
2023-01-24 11:34:40 -0500

    added new part time student class

---
+++ src/parttimestudent.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author aliibrahim
+ */
+public class parttimestudent extends StudentW23TueAM {
+    
+          private int numofCourses;
+
+    public parttimestudent(int numofCourses) {
+        this.numofCourses = numofCourses;
+    }
+
+    public int getNumofCourses() {
+        return numofCourses;
+    }
+
+    public void setNumofCourses(int numofCourses) {
+        this.numofCourses = numofCourses;
+    }
+          
+}
